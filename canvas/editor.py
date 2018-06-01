2cae43934cead3ec900df4bed927106902b35d4c
Hemang Kandwal <2285702k@student.gla.ac.uk>
2018-06-01 18:33:45 +0100

    Editor can now scroll vertically to add more content. It is beautiful to see...

--- canvas/editor.py
+++ canvas/editor.py
@@ -14,6 +14,7 @@ class Editor:
         self.number_toolbar_width = 6
         self.height, self.width = self.std_scr.getmaxyx()
         self.origin_x, self.origin_y = self.width//4+self.number_toolbar_width, 4
+        self.relative_x, self.relative_y = 0, 0
         self.canvas_height = self.height-1-self.origin_y-1
         self.canvas_width = self.width-2-self.origin_x-1
         self.navigator = None
@@ -23,10 +24,13 @@ class Editor:
 
     def analyser(self):
         if self.last_key_pressed == curses.KEY_UP:
-            self.cursor_y = self.cursor_y-1 if self.cursor_y >=0 else -1
-            if self.cursor_y == -1:
-                self.go_to_menu()
-                return
+            if self.cursor_y - self.relative_y == 0 and self.relative_y > 0:
+                self.relative_y -= 1
+                self.cursor_y = self.cursor_y - 1 if self.cursor_y > 0 else 0
+                self.update_number_toolbar()
+                self.update_existing_data()
+            else:
+                self.cursor_y = self.cursor_y - 1 if self.cursor_y > 0 else 0
             if self.data[self.cursor_y]=="":
                 self.cursor_x=0
             elif len(self.data[self.cursor_y]) < len(self.data[self.cursor_y+1]):
@@ -34,12 +38,18 @@ class Editor:
                 self.cursor_x = len(self.data[self.cursor_y])
         elif self.last_key_pressed == curses.KEY_DOWN:
             if len(self.data)-1>self.cursor_y:
-                self.cursor_y += 1
-                if self.data[self.cursor_y]=="":
-                    self.cursor_x=0
-                elif len(self.data[self.cursor_y]) < len(self.data[self.cursor_y-1]):
-                    # to go to the back of line if the lower line doesn't have many characters
-                    self.cursor_x = len(self.data[self.cursor_y])
+                if self.cursor_y - self.relative_y == self.canvas_height:
+                    self.relative_y += 1
+                    self.cursor_y = self.cursor_y + 1 if self.cursor_y < len(self.data)-1 else len(self.data)-1
+                    self.update_number_toolbar()
+                    self.update_existing_data()
+                else:
+                    self.cursor_y = self.cursor_y + 1 if self.cursor_y < len(self.data)-1 else len(self.data)-1
+                    if self.data[self.cursor_y]=="":
+                        self.cursor_x=0
+                    elif len(self.data[self.cursor_y]) < len(self.data[self.cursor_y-1]):
+                        # to go to the back of line if the lower line doesn't have many characters
+                        self.cursor_x = len(self.data[self.cursor_y])
         elif self.last_key_pressed == curses.KEY_LEFT:
             if self.cursor_x > 0:
                 self.cursor_x-=1
@@ -54,70 +64,91 @@ class Editor:
                 self.cursor_y+=1
                 self.cursor_x=0
         elif 32 <= self.last_key_pressed <= 126:
+            y, x = self.cursor_y+self.origin_y-self.relative_y, self.cursor_x+self.origin_x+BAR_OFFSET
             self.data[self.cursor_y]= self.data[self.cursor_y][:self.cursor_x]+chr(self.last_key_pressed)+self.data[self.cursor_y][self.cursor_x:]
-            self.std_scr.addstr(self.cursor_y+self.origin_y,self.cursor_x+self.origin_x+BAR_OFFSET,self.data[self.cursor_y][self.cursor_x:])
+            self.update_existing_data()
             # update the cursors
             self.cursor_x += 1
         elif self.last_key_pressed == ENTER:
             # shorten the line of cursor at y
-            y,x = self.cursor_y+self.origin_y,self.origin_x+self.cursor_x
+            y,x = self.cursor_y+self.origin_y-self.relative_y,self.origin_x+self.cursor_x
             buffer_end_line = self.data[self.cursor_y][self.cursor_x:]
 
-            # empty the next lines to easily refresh them
-            for i in range(self.cursor_y+1,len(self.data)):
-                self.std_scr.addstr(i+self.origin_y,self.origin_x+BAR_OFFSET," "*len(self.data[i]))
-
             self.data[self.cursor_y] = self.data[self.cursor_y][:self.cursor_x]
-            self.data = self.data[:self.cursor_y+1]+[buffer_end_line]+self.data[self.cursor_y+1:]
-            self.std_scr.addstr(y,x+1," "*len(buffer_end_line))
-            self.std_scr.addstr(y+1,self.origin_x+BAR_OFFSET,self.data[self.cursor_y+1])
-            # delete below the cursor y
+            self.data = self.data[:self.cursor_y + 1] + [buffer_end_line] + self.data[self.cursor_y + 1:]
+            self.std_scr.addstr(y, x + 1, " " * len(buffer_end_line))
+            self.std_scr.addstr(y + 1, self.origin_x + BAR_OFFSET, self.data[self.cursor_y + 1])
+
+            if self.cursor_y - self.relative_y == self.canvas_height:
+                self.relative_y += 1
             self.cursor_y += 1
-            for i in range(self.cursor_y,len(self.data)):
-                self.std_scr.addstr(i+self.origin_y,self.origin_x+BAR_OFFSET,self.data[i])
-            self.update_number_toolbar()
             self.cursor_x = 0
+            self.update_existing_data()
+            self.update_number_toolbar()
         elif self.last_key_pressed == curses.KEY_BACKSPACE:
             if self.cursor_x==0:
                 if len(self.data[self.cursor_y]) > 0:
                     self.data[self.cursor_y] = self.data[self.cursor_y][:-1]
-                if self.cursor_y > 0:
-                    # check if y is last and then delete to update the right numbering in the number toolbar
-                    if self.cursor_y == len(self.data)-1:
-                        y, x = self.origin_y+len(self.data)-1, self.origin_x-self.number_toolbar_width+1
-                        for i in range(self.number_toolbar_width):
-                            self.std_scr.addstr(y,x+i," ")
-                        self.data = self.data[:-1]
-                    self.std_scr.addstr(self.cursor_y+self.origin_y, self.cursor_x+self.origin_x," ")
-                    self.cursor_y-=1
+                elif self.cursor_y > 0:
+                    # delete the line as it contains nothing
+                    del self.data[self.cursor_y]
+                    if self.cursor_y - self.relative_y <= self.canvas_height-3 and self.relative_y > 0:
+                        self.relative_y -= 1
+                    self.cursor_y = self.cursor_y - 1 if self.cursor_y > 0 else 0
                     self.cursor_x = len(self.data[self.cursor_y])
+                    self.update_number_toolbar()
+                    self.update_existing_data()
             else:
                 self.data[self.cursor_y] = self.data[self.cursor_y][:-1]
-                self.std_scr.addch(self.cursor_y + self.origin_y, self.cursor_x + self.origin_x, " ")
-                self.cursor_x-=1
+                self.std_scr.addch(self.cursor_y-self.relative_y + self.origin_y, self.cursor_x -self.relative_x + self.origin_x, " ")
+                self.cursor_x -= 1
         elif self.last_key_pressed == CTRL_X:
             import sys
             sys.exit(0)
 
+    def empty_number_toolbar(self):
+        y, x = self.origin_y, self.origin_x-self.number_toolbar_width+1
+        for i in range(self.canvas_height+1):
+            self.std_scr.addstr(y+i,x," "*(self.number_toolbar_width-1))
+
+    def empty_canvas(self):
+        y, x = self.origin_y, self.origin_x+BAR_OFFSET
+        for i in  range(self.canvas_height+1):
+            self.std_scr.addstr(y+i,x," "*(self.canvas_width))
+
     def update_number_toolbar(self):
-        y,x = self.origin_y, self.origin_x-self.number_toolbar_width+1
-        for line_num in range(len(self.data)):
+        self.empty_number_toolbar()
+        y,x = self.origin_y-self.relative_y, self.origin_x-self.number_toolbar_width+1
+        height_index = 0
+        line_num = self.relative_y
+        while line_num < len(self.data):
             self.std_scr.addstr(y+line_num,x,str(line_num+1))
             self.std_scr.addstr(y+line_num,x+self.number_toolbar_width-2,"|")
+            line_num += 1
+            if height_index == self.canvas_height:
+                break
+            else:
+                height_index += 1
 
     def update_existing_data(self):
-        for y in range(len(self.data)):
-            self.std_scr.addstr(y+self.origin_y,self.origin_x+BAR_OFFSET,self.data[y])
-
-
+        self.empty_canvas()
+        y,x = self.origin_y-self.relative_y, self.origin_x+BAR_OFFSET
+        height_index = 0
+        line_num = self.relative_y
+        while line_num < len(self.data):
+            self.std_scr.addstr(line_num+y,x,self.data[line_num])
+            line_num += 1
+            if height_index == self.canvas_height:
+                break
+            else:
+                height_index += 1
     def edit(self):
         self.update_existing_data()
         while not self.exit:
             self.update_number_toolbar()
-            self.std_scr.move(self.origin_y+self.cursor_y,self.origin_x+self.cursor_x+BAR_OFFSET)
+            self.std_scr.move(self.origin_y+self.cursor_y-self.relative_y,self.origin_x+self.cursor_x-self.relative_x+BAR_OFFSET)
             self.last_key_pressed = self.std_scr.getch()
             self.analyser()
-
 
     def update_global_status(self,status):
         self.is_global_state = status
